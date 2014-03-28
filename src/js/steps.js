36e33ff082f6b9f8c88948c075c0422588253445
Micah Blu <micahblu@gmail.com>
2014-03-27 17:18:15 -0700

    Added event handling for panel collapse

--- src/js/steps.js
+++ src/js/steps.js
@@ -1,9 +1,27 @@
 
 (function($){
 	$.fn.steps = function(data) {
+
+		for(var i=0; i < data.steps.length; i++){
+			// add id to each object
+			data.steps[i].id = i;
+		}
+
 		var source = this.html();
 		var template = Handlebars.compile(source);
-		console.log(template);
+
 		this.after(template(data));
+
+		$(".panel-header").on('click', function(e){
+			if($(this.getAttribute("data-target")).find('.panel-body').hasClass('collapse')){
+				$('.panel-body').addClass('collapse');
+				$(this.getAttribute("data-target")).find('.panel-body').removeClass('collapse');
+			}
+		});
+
+		function contains(needle, str){
+			var patt = new RegExp(needle);
+			return patt.test(str);
+		}
 	};
 }(jQuery));
