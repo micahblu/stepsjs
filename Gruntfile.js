4bd6623f91708af4607e893f7e8632926559bdb9
Micah Blu <micahblu@gmail.com>
2014-06-27 14:52:38 -0700

    Fixed typo bug on registerHelper method, removed unused grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -85,9 +85,10 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-connect');
+  
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-copy');
--- src/js/steps.js
+++ src/js/steps.js
@@ -4,7 +4,7 @@
  * 
  * @author : micahblu | micahblu.com | github.com/micahblu
  * @license http://opensource.org/licenses/MIT MIT License
- * @version 0.1.0
+ * @version 0.1.1
  * 
  */
 (function($){
@@ -101,7 +101,7 @@
 	 *
 	 * @return void
 	 */
-	function regiesterHelpers(){
+	function registerHelpers(){
 		
 		/**
 		 * Handlebars 'select' Helper
